f43551557f1f2f87b453b586ef11e493c36a85da
chintastic1 <chintastic1@yahoo.com>
2015-10-22 18:01:27 -0500

    10/22/2015 GUI

--- src/PiGUI/main.py
+++ src/PiGUI/main.py
@@ -3,16 +3,23 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
+from kivy.uix.dropdown import DropDown
 
 
 class centeralLayout(Widget):
     def topLayout(self):
-        pageLayout = GridLayout()
-        menu = BoxLayout()
-        pageLayout.add_widget(menu)
-        menu1_3 = BoxLayout(cols = 5, rows = 1,size_hint=(1,1))
+        masterPageLayout = BoxLayout(orientation = 'vertical', rows = 2)
+        
+        menu = BoxLayout(cols = 3 )
+        masterPageLayout.add_widget(menu)
+        
+        page = GridLayout()
+        masterPageLayout.add_widget(page)
+        
+        menu1_3 = BoxLayout(cols = 5, rows = 1,size_hint=(1,.25))
         menu2_3 = BoxLayout() #blank 2/3 of menu
         menu3_3 = BoxLayout() #blank 3/3 of menu
+        
         menu.add_widget(menu1_3)
         menu.add_widget(menu2_3)
         menu.add_widget(menu3_3)
@@ -25,8 +32,7 @@ class centeralLayout(Widget):
         menu1_3.add_widget(editButton)
         menu1_3.add_widget(helpButton)
         
-        page = GridLayout()
-        pageLayout.add_widget(page)
+
         '''
         
         graphics attributes for main page goes here
@@ -34,7 +40,7 @@ class centeralLayout(Widget):
         
         '''
         
-        return pageLayout
+        return masterPageLayout
         
         
     

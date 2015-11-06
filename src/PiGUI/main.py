0eeb08cd6a6642a57f33180e7e5c1a8d825104f2
Mitchellwbooks <mitchellwbooks@gmail.com>
2015-11-05 18:07:06 -0600

    11/05/2015 6:05pm
    moving to kv styling

--- src/PiGUI/main.py
+++ src/PiGUI/main.py
@@ -4,8 +4,14 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.dropdown import DropDown
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.lang import Builder
+
+class dashboard(Widget):
+    pass
 
 
+'''
 class centeralLayout(Widget):
     def topLayout(self):
         masterPageLayout = BoxLayout(orientation = 'vertical', rows = 2)
@@ -29,11 +35,10 @@ class centeralLayout(Widget):
         editButton = Button(text = "edit")
         helpButton = Button(text = "help")
         
-        '''
+      
             10/22/2015 6:50 PM
                 Test buttons, not sure of function response yet   
-                        
-        '''
+
         fileButton2 = Button(text='Value %d' % 1, size_hint_y=None, height=44)
         fileButton3 = Button(text='Value %d' % 2, size_hint_y=None, height=44)
         
@@ -51,22 +56,20 @@ class centeralLayout(Widget):
         menuLeft.add_widget(editButton)
         menuLeft.add_widget(helpButton)
         
-
-        '''
         
         graphics attributes for main page goes here
         ...we should also learn some .kv for styling so we don't need to write attributes in file
         
-        '''
+        
         
         return masterPageLayout
+        '''
         
-        
-    
+finalLayout = Builder.load_file("my.kv")
 
-class myApp(App,centeralLayout):
+class myApp(App):
     def build(self):
-        finalLayout = super(myApp,self).topLayout()
+        #finalLayout = super(myApp,self).topLayout()
         return finalLayout
     
 myApp().run()
